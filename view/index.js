ee221b7f7b4a1f60c8627f83a5c8c064c793cd1d
Pedro Oliveira <pjmc.oliveira@gmail.com>
2019-01-26 15:22:42 -0500

    Made template for login and register functions

--- view/index.js
+++ view/index.js
@@ -1,23 +1,31 @@
-window.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function() {
     console.log('Loaded!');
     setup();
 });
 
 function setup() {
-    const loginBtn = document.getElementById('login-btn');
-    loginBtn.addEventListener('click', event => {
-        event.preventDefault();
-        console.log(event);
-    });
-
     const loginForm = document.getElementById('login-form');
     loginForm.addEventListener('submit', event => {
         event.preventDefault();
-        console.log(event);
+        const form = event.target;
+        login(form.username.value, form.password.value);
+    });
+
+    const registerForm = document.getElementById('register-form');
+    registerForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const form = event.target;
+        register(form.username.value, form.password.value, form.email.value);
     });
 }
 
 function login(username, password) {
     console.log(`Username: ${username}`);
     console.log(`Password: ${password}`);
+}
+
+function register(username, password, email) {
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
+    console.log(`Email: ${email}`);
 }
